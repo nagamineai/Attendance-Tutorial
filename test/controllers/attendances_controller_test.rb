192037fcf1483d759d0f4aa000393fdeb839f4a5
EC2 Default User <ec2-user@ip-172-31-29-123.ap-northeast-1.compute.internal>
2021-08-29 06:11:31 +0000

    Finish add-user-attendances

--- test/controllers/attendances_controller_test.rb
+++ test/controllers/attendances_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class AttendanceControllerTest < ActionDispatch::IntegrationTest
+class AttendancesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
