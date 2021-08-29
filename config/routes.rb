29e98d4492ad323430ed445f2dd845eb8f0f9ba8
EC2 Default User <ec2-user@ip-172-31-29-123.ap-northeast-1.compute.internal>
2021-08-29 13:38:41 +0000

    Finish edit-attendanses

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
     member do
       get 'edit_basic_info'
       patch 'update_basic_info'
+      get'attendances/edit_one_month'#追加
+      patch'attendances/update_one_month'#追加の追加
     end
     resources :attendances, only: :update # この行を追加します。
   end
